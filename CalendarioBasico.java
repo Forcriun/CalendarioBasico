9981010239228e5d11f8b79a00b37b38c3040c4e
Forcriun <forcriun@gmail.com>
2017-11-01 13:55:22 +0100

    Creada la clase CalendarioBasico con su constructor

---
+++ CalendarioBasico.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class CalendarioBasico here.
+ *
+ * @author (Dídac Fernández Fernández)
+ * @version (a version number or a date)
+ */
+public class CalendarioBasico
+{
+    // instance variables - replace the example below with your own
+    private int day;
+    private int month;
+    private int year;
+
+    /**
+     * Constructor for objects of class CalendarioBasico
+     */
+    public CalendarioBasico()
+    {
+        // initialise instance variables
+        day = 1;
+        month = 1;
+        year = 1;
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return y;
+    }
+}
